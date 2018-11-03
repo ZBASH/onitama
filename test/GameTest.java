33f7b0adba7e4839ff7ba2b540d6c94814316f74
bashbashbash <7basch@gmail.com>
2018-11-02 21:02:10 -0500

    Backfilled Game tests

--- test/GameTest.java
+++ test/GameTest.java
@@ -4,8 +4,22 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class GameTest {
     @Test
-    void itRuns() {
-        String board = "board";
-        assertEquals("board", board);
+    void itHasAFirstPlayer() {
+       Game game = new Game();
+       game.start();
+
+       Player player = game.getPlayers().get(0);
+       assertEquals(0, player.getPawns().get(0).getY());
+       assertEquals(Color.RED, player.getColor());
+    }
+
+    @Test
+    void itHasASecondPlayer() {
+        Game game = new Game();
+        game.start();
+
+        Player player = game.getPlayers().get(1);
+        assertEquals(4, player.getPawns().get(0).getY());
+        assertEquals(Color.BLUE, player.getColor());
     }
 }

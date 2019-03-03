b2bcaef5a2f71ffdbd60924a6da68ec66337c468
ZBASH <zlabas2@uic.edu>
2019-03-02 23:32:12 -0600

    burn it down

--- src/main/java/core/Color.java
+++
@@ -1,7 +0,0 @@
-package core;
-
-public enum Color {
-    NONE,
-    RED,
-    BLUE
-}
--- src/main/java/core/Config.java
+++
@@ -1,6 +0,0 @@
-package core;
-
-public final class Config {
-    public static final int  BOARD_SIZE  = 5;
-    public static final char BOARD_SPACE = '*';
-}
